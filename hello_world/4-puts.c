514cdb1d9b3ccc779b82fa81d2606d2a9e898e47
NicolasDS83600 <nico836001@outlook.fr>
2025-10-21 14:51:22 +0200

    Correction du 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,6 +3,6 @@
 int main()
 {
 int puts (const char * string);
-puts ("\"Pogramming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 return 0;
 }
