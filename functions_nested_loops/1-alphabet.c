701a1e3169db448a6b83de766e47b001c83bd48c
NicolasDS83600 <nico836001@outlook.fr>
2025-10-24 15:14:03 +0200

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 char c;
 
@@ -14,5 +14,4 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 }
 _putchar('\n');
-return (0);
 }
