70a1c4004d500d1f1cb5618039a0d9750fcd9d3b
NicolasDS83600 <nico836001@outlook.fr>
2025-10-27 14:55:26 +0100

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@
 void more_numbers(void)
 {
 int r, n;
-for (r = 0; r < 10; r++)
-{
-for (n = 0; n <= 14; n++)
-{
-if (n >= 10)
-_putchar(n / 10 + '0');
- _putchar((n % 10) + '0');
-}
-_putchar('\n');
-}
+	for (r = 0; r < 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+		if (n >= 10)
+		_putchar(n / 10 + '0');
+		_putchar((n % 10) + '0');
+		}
+	_putchar('\n');
+	}
 }
