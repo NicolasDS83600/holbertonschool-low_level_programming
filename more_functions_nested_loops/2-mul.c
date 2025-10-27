164bf7c69b5dfcaf0242f1f5085c03aace7c8d96
NicolasDS83600 <nico836001@outlook.fr>
2025-10-27 11:28:00 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: The character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int mul(int a, int b)
+  int a, b;
+{
+  return a * b;
+}
