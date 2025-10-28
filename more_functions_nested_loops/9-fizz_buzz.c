c34d654903e9833a1194f14ac3a4acbacc110d87
NicolasDS83600 <nico836001@outlook.fr>
2025-10-28 14:14:07 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,17 +6,18 @@ int n;
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-		{
 			printf("FizzBuzz");
 			else if (n % 5 == 0)
 			printf("Buzz");
 			else if (n % 3 == 0)
 			printf("Fizz");
 			else
-			putchar(n);
-		}
-		putchar(' ');
+			printf("%d", n);
 	}
+		if (n < 100)
+			putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
   
