b10312f400a561da3ec85bf306474c2dc18d090b
NicolasDS83600 <nico836001@outlook.fr>
2025-10-28 19:27:20 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the numbers from 1 to 100, replacing multiples
+ *        of 3 with Fizz, multiples of 5 with Buzz, and multiples
+ *        of both 3 and 5 with FizzBuzz.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 int n;
