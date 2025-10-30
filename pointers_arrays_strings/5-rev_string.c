466d830e688f5ae9fbefbf64d19ce815dfd6bd5a
NicolasDS83600 <nico836001@outlook.fr>
2025-10-30 14:10:59 +0100

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,13 +4,12 @@ void rev_string(char *s)
 {
 char *start = s;
 char *end = s;
-while (*start != '\0')
+while (*end != '\0')
 {
 end++;
 end--;
 
 while (start < end)
-{
 char temp = *start;
 *start = *end;
 *end = temp;
