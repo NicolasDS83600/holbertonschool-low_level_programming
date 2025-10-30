8149fbd3d848bf341b894330366840b50443e510
NicolasDS83600 <nico836001@outlook.fr>
2025-10-30 21:10:53 +0100

    Correction of the int & the loop increment, in 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * puts2 - prints every other character of a string,
+ *         starting with the first character,
+ *         followed by a new line.
+ * @str: the input string
+ *
+ * Return: nothing
+ */
 void puts2(char *str)
 {
 int i = 0;
