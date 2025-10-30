afac853c2a10e074e6174b89fe61a14f702c6d8f
NicolasDS83600 <nico836001@outlook.fr>
2025-10-30 05:47:13 +0100

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,7 +8,7 @@
  * Description: Cette fonction prend deux pointeurs vers des entiers et échange
  * les valeurs des entiers pointés.
  */
-void swap_int(int *a, int *b)  
+void swap_int(int *a, int *b)
 {
 int i = *a;
 *a = *b;
