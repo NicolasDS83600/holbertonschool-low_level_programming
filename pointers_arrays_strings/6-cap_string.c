66454d3ee1fec16cd3834a621424d23c1c080114
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 19:59:27 +0100

    Correction of the return in 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,14 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-	if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
 
 		for (j = 0; sym[j] != '\0'; j++)
 		{
 
-		if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 			{
 				str[i + 1] -= 32;
 				break;
@@ -24,5 +25,5 @@ char *cap_string(char *str)
 		i++;
 	}
 
-	return str);
-
+	return (str);
+}
