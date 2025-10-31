dc28d8deb3f5c1c721ac6717bf92d0942e7041d5
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 14:52:23 +0100

    Correction de Betty style in 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,26 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-unsigned int n = 0;
-int r = 0;
+	int i = 0, res;
+	int sign = 1;
+	unsigned int n = 0;
+	int r = 0;
 
-while (s[i] != '\0')
-{
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] == '+')
-sign *= 1;
-else if (s[i] >= '0' && s[i] <= '9')
-{
-r = 1;
-n = (n * 10) + (s[i] - '0');
-}
-else if (r)
-break;
-i++;
-}
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+	else if (s[i] == '+')
+	sign *= 1;
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+	r = 1;
+	n = (n * 10) + (s[i] - '0');
+	}
+	else if (r)
+	break;
+	i++;
+	}
+
 return (sign * n);
 }
