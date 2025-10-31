6bc0f3dffb85db76ff9786376b4a2f854a4043b2
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 17:58:51 +0100

    Correction of Betty style in 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-
+/**
+ * _strncpy - Copies a string
+ * @dest: The destination buffer
+ * @src: The source string
+ * @n: The maximum number of bytes to copy from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
