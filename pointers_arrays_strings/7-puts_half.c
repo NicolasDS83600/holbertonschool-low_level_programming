e3927a4fd02ac7a10b3b68d4b3b5ab3f1750e5e8
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 01:21:10 +0100

    Correction of Betty style in 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * puts_half - prints the second half of a string,
+ * followed by a new line
+ * @str: the string to print
+ *
+ * Description: If the number of characters is odd,
+ * the function prints the last n characters of the string,
+ * where n = (length_of_the_string + 1) / 2.
+ */
 void puts_half(char *str)
 {
 int len = 0;
