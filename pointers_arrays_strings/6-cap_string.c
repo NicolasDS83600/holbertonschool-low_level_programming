753ff78572f8d3b7d2c996950d83002dbfba70ab
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 19:56:11 +0100

    Addition of 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char sym[] = " \t\n,;.!?\"(){}";
+
+	while (str[i] != '\0')
+	{
+	if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+
+		for (j = 0; sym[j] != '\0'; j++)
+		{
+
+		if (str[i] == sep[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] -= 32;
+				break;
+			}
+		}
+
+		i++;
+	}
+
+	return str);
+
