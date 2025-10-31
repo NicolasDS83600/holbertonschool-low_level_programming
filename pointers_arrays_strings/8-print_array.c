799ff4875e668c15c4078c52e87eb4e93be50189
NicolasDS83600 <nico836001@outlook.fr>
2025-10-31 07:02:35 +0100

    Correction of Betty style in 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: pointer to the array of integers
+ * @n: number of elements of the array to be printed
+ *
+ * Description: The numbers are separated by a comma and a space,
+ * and they are printed in the same order as they are stored
+ * in the array.
+ */
 void print_array(int *a, int n)
 {
 int i;
