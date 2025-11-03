d75afb466cb4fd71b73e8e4652456b311b4acad4
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 10:21:22 +0100

    Correction of Betty style n°2 in 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill with
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 
