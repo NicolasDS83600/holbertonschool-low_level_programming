374b0ed9a27f398763844e6ef35ce919eb5da598
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 11:52:23 +0100

    Correction of 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,19 @@
 char *_strchr(char *s, char c)
 {
 
-	{
-
 	int i = 0;
 
+	{
 		while (s[i] != '\0')
 
 		if (s[i] == c)
-		return (s);
-
-		else
-		return(0);
-
+		return (s + i);
 		i++;
+
 	}
+
+		if (c == '\0')
+		return(s + i);
 
 	return(0);
 }
