1c65e8a577e0c995f1cc2936682c150894030be2
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 14:23:49 +0100

    Correction of the Betty stylein 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String to search
+ * @accept: Characters to match
+ *
+ * Return: Number of bytes in the initial segment of s
+ *         consisting only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int len = 0;
