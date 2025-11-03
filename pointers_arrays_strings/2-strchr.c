9b8249eda509ce11c2ff9460d92078e370d21b1c
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 11:16:57 +0100

    Addition of 2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char *_strchr(char *s, char c)
+{
+
+	{
+
+	int i = 0;
+
+		while (s[i] != '\0')
+
+		if (s[i] == c)
+		return (s);
+
+		else
+		return(0);
+
+		i++;
+	}
+
+	return(0);
+}
