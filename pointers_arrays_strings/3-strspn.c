097b027c1fad13b978958e6490549f8fb0c42df6
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 14:19:50 +0100

    Addition of 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned len = 0;
+	int i = 0, j;
+	int count;
+
+	while (s[i] != '\0')
+	{
+		count = 0;
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+			count = 1;
+			break;
+			}
+			j++;
+		}
+		if (!count)
+		break;
+		len++;
+		i++;
+	}
+	return (len);
+}
+
