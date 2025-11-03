df5be2a62017522009dda2c5d5f1beb008c5f03b
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 10:46:43 +0100

    Correction of Betty style in 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
+ * Description: Copies n bytes from memory area src to memory area dest.
+ * Return: Pointer to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
