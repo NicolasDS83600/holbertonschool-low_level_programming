5059bb88c7e5e071a45aff9fa8f0b4b40a376488
NicolasDS83600 <nico836001@outlook.fr>
2025-11-03 16:00:37 +0100

    Correction of the Betty style in 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strstr - Locates a substring in a string
+ * @haystack: The string to be searched
+ * @needle: The substring to find
+ *
+ * Return: A pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	char *h, *n;
