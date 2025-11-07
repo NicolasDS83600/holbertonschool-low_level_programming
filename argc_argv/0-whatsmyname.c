6ae2d84227fc4ed0b8b8071f412c587e9ead2b45
NicolasDS83600 <nico836001@outlook.fr>
2025-11-07 13:23:26 +0100

    Addition of 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program, including its path
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char* argv[])
+{
+
+	(void)argc;
+	printf(argv[0]);
+	return (0);
+}
