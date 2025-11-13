c1ba474d118f8274c0262e1f5a509c5df874523c
NicolasDS83600 <nico836001@outlook.fr>
2025-11-13 13:32:56 +0100

    Correction of the condition if in 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "dog.h"
 
-
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function sets the name, age, and owner fields
+ * of the struct dog pointed to by d. It does not allocate memory,
+ * it only assigns the pointers directly.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (d == NULL)
+	return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 }
