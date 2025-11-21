bbf1f0a4022855fc78156bbd61484d6507ff7506
NicolasDS83600 <nico836001@outlook.fr>
2025-11-21 13:10:42 +0100

    refactor(3-print_all.c): split single function into two

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,61 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-/**
-* print_all - prints anything according to a format string
-* @format: list of types of arguments passed to the function
-*          (c = char, i = int, f = float, s = char *)
-*/
+void print_one(char c, va_list args)
+{
+	char *s;
+
+	switch (c)
+	{
+		case 'c':
+		printf("%c", va_arg(args, int));
+		break;
+
+		case 'i':
+		printf("%i", va_arg(args, int));
+		break;
+
+		case 'f':
+		printf("%f", va_arg(args, double));
+		break;
+
+		case 's':
+		s = va_arg(args, char *);
+		if (s == NULL)
+		s = "(nil)";
+		printf("%s", s);
+		break;
+	}
+}
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0, sep = 0;
-	char *s;
+	int i = 0;
+	int sep = 0;
 
 	va_start(args, format);
+
 	while (format && format[i])
 	{
-		if (sep)
-		printf(", ");
 
-		switch (format[i])
+		if (format[i] == 'c' || format[i] == 'i' ||
+		format[i] == 'f' || format[i] == 's')
 		{
-			case 'c':
-			printf("%c", va_arg(args, int));
+
+			while (sep == 1)
+			{
+				printf(", ");
+				sep = 0;
+			}
+
+			print_one(format[i], args);
 			sep = 1;
-			break;
-			case 'i':
-			printf("%d", va_arg(args, int));
-			sep = 1;
-			break;
-			case 'f':
-			printf("%f", va_arg(args, double));
-			sep = 1;
-			break;
-			case 's':
-			s = va_arg(args, char *);
-			if (!s)
-			printf("(nil)");
-			printf("%s", s);
-			sep = 1;
-			break;
-			default:
-			sep = 0;
-			break;
 		}
+
 		i++;
 	}
-	printf("\n");
+
 	va_end(args);
+	printf("\n");
 }
