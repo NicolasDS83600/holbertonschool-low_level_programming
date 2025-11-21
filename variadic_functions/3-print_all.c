2d0a1e5885ecf7d4b199568aa6e62ade1665f52a
NicolasDS83600 <nico836001@outlook.fr>
2025-11-21 06:07:47 +0100

    refactor: simplify and shorten 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,73 +9,41 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	const char *p;
-	int d = 0;
+	int i = 0, sep = 0;
+	char *s;
 
 	va_start(args, format);
-	p = format;
-
-	while (p != NULL && *p != '\0')
+	while (format && format[i])
 	{
-		switch (*p)
+		if (sep)
+		printf(", ");
+
+		switch (format[i])
 		{
 			case 'c':
-			{
-				char c = (char) va_arg(args, int);
-
-				if (d)
-				printf(", ");
-				printf("%c", c);
-				d = 1;
-				break;
-			}
-
+			printf("%c", va_arg(args, int));
+			sep = 1;
+			break;
 			case 'i':
-			{
-				int i = va_arg(args, int);
-
-				if (d)
-				printf(", ");
-				printf("%d", i);
-				d = 1;
-				break;
-			}
-
+			printf("%d", va_arg(args, int));
+			sep = 1;
+			break;
 			case 'f':
-			{
-				double d = va_arg(args, double);
-
-				if (d)
-				printf(", ");
-				printf("%f", d);
-				d = 1;
-				break;
-			}
-
+			printf("%f", va_arg(args, double));
+			sep = 1;
+			break;
 			case 's':
-			{
-				char *s = va_arg(args, char *);
-
-				if (d)
-				printf(", ");
-
-				if (s == NULL)
-				printf("(nil)");
-
-				else
-				printf("%s", s);
-				d = 1;
-				break;
-			}
-
+			s = va_arg(args, char *);
+			if (!s)
+			s = "(nil)";
+			printf("%s", s);
+			sep = 1;
+			break;
 			default:
 			break;
 		}
-
-		p++;
+		i++;
 	}
-
-	va_end(args);
-
 	printf("\n");
+	va_end(args);
 }
